f0a2abc58f07d33d5f298d6ff124d53197a54c90
danielrecto-art <daniel.recto@gmail.com>
2026-07-22 18:18:29 +0100

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from supabase import create_client, Client
 # ==========================================
 # ✏️ Altera aqui o nome do teu estabelecimento e o link do logótipo
 NOME_ESTABELECIMENTO = "Café Triangulo"  # Altera para o nome pretendido
-LOGO_URL = "https://cdn-icons-png.flaticon.com/512/3075/3075977.png"  # Link para a tua imagem/logo (PNG)
+LOGO_URL = "https://github.com/danielrecto-art/snackbar-atendente/blob/main/file_000000005f0881f49a73ed28f23b0776.png"  # Link para a tua imagem/logo (PNG)
 
 # Configuração da Página do Streamlit
 st.set_page_config(
